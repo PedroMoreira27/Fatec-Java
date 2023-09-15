a6b94bcb1d0ef8162303f71c7ee1790cd5b2acea
Pedro Gonçalves Moreira <111304982+PedroMoreira27@users.noreply.github.com>
2023-09-14 21:57:18 -0300

    Add files via upload

---
+++ ExercicioAvaliativo/src/main/java/pedro/exercicioavaliativo/Exercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pedro.exercicioavaliativo;
+
+/**
+ *
+ * @author 1091392313013
+ */
+public class Exercicio1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            
+            System.out.println("Insira o genero: ");
+            genero = input.nextLine();
+
+    }
+}
